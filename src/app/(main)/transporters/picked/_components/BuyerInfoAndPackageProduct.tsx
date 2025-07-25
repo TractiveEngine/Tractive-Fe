7f3dbff4b3419087df40434d4e8d975260a1ece9
JoeTechx <oyinjoe23@gmail.com>
2025-07-25 11:56:53 +0100

    Feat: All Trancking pages built

--- src/app/(main)/transporters/picked/_components/BuyerInfoAndPackageProduct.tsx
+++ src/app/(main)/transporters/picked/_components/BuyerInfoAndPackageProduct.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React from "react";
 import { PackagedTable } from "./PackagedTable";
 import { PhoneCall } from "../../_components/Icons/TransporterIcons";
 
